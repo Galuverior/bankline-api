f77ef345c56e1f0e263620fe5d157ab6342c5d67
Eduardo Fragoso <duds.fragoso@gmail.com>
2022-05-17 19:33:54 -0300

    mapeando as entidades com jpa

--- src/main/java/model/Correntista.java
+++ src/main/java/model/Correntista.java
@@ -1,13 +1,22 @@
 package model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tab_correntista")
 public class Correntista {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(length = 20)
     private String cpf;
 
+    @Column(length = 60)
     private String nome;
 
+    @Embedded
     private Conta conta;
 
     public Conta getConta() {
